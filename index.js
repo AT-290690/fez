e30aaf9ffa4a18d6492e7c0e814d5555a44d0fd0
AnthonyTonev <32884987+AnthonyTonev@users.noreply.github.com>
2025-01-01 02:02:09 +0200

    Add destructuring

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 import { evaluate } from './src/evaluator.js'
 import { LISP, AST } from './src/parser.js'
-import { fez, tree, ast, src, js, prep, dependencies } from './src/utils.js'
+import { fez, prep } from './src/utils.js'
 import std from './lib/baked/std.js'
-import { keywords } from './src/interpreter.js'
-import { WORD, APPLY, ATOM, VALUE, TYPE } from './src/keywords.js'
-const types = { WORD, APPLY, ATOM, VALUE, TYPE }
-export {
-  fez,
-  keywords,
-  evaluate,
-  std,
-  types,
-  tree,
-  ast,
-  src,
-  js,
-  prep,
-  dependencies,
-  LISP,
-  AST
-}
+export { fez, evaluate, std, prep, LISP, AST }
