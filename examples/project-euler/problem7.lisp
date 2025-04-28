358baa5f8b6272c726e6e705620519dbb4b51ab9
Anthony <anthony@Anthonys-MacBook-Pro.local>
2025-04-28 14:16:34 +0300

    Add a more functional solution to problem 7

--- examples/project-euler/problem7.lisp
+++ examples/project-euler/problem7.lisp
@@ -1,5 +1,5 @@
-
-(let count (math:var-def 2))
-(let number (math:var-def 3))
-(loop (!= (math:var-get count 10001)) (if (math:prime? (math:var-add-and-get! number 2)) (math:var-increment! count)))
-(math:var-get number)
+(let recursive:solve (lambda count number
+    (if (= count 10001)
+        number
+        (recursive:solve (if (math:prime? (+ number 2)) (+ count 1) count) (+ number 2)))))
+(recursive:solve 2 3)
