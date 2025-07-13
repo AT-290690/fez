17630796371e4f69742a36de8c3a6100f238b290
Anthony <anthony@Anthonys-MacBook-Pro.local>
2025-07-14 00:08:21 +0300

    Renew link

--- miscs/link/main.js
+++ miscs/link/main.js
@@ -1,14 +1,18 @@
-import { dev, link } from './link-function.js'
+import { comp, dev, link } from './link-function.js'
 import { readFileSync } from 'fs'
 
+const file = readFileSync('./playground/main.lisp', 'utf-8')
 switch (process.argv[2]) {
+  case 'comp':
+    comp(file)
+    break
   case 'link':
-    link(readFileSync('./playground/main.lisp', 'utf-8'))
+    link(
+      file
+      // 'http://127.0.0.1:3000/lambda.html'
+    )
     break
   case 'dev':
-    dev(
-      readFileSync('./playground/main.lisp', 'utf-8'),
-      readFileSync('./playground/main-sig.lisp', 'utf-8')
-    )
+    dev(file, readFileSync('./playground/main-sig.lisp', 'utf-8'))
     break
 }
