3652f37ec79818f92fec7675f0e72d8ebabfaaab
Anthony <anthony@Anthonys-MacBook-Pro.local>
2025-07-27 23:15:04 +0300

    Remove different inputs form examples

--- examples/2018/day2.lisp
+++ examples/2018/day2.lisp
@@ -1,11 +1,3 @@
-(let INPUT "abcdef
-bababc
-abbcde
-abcccd
-aabcdd
-abcdee
-ababab")
-
 (let parse (lambda input (|> input (from:string->array char:new-line))))
 
 (let part1 (lambda input (do 
@@ -27,27 +19,32 @@ ababab")
         (array:reduce entries (lambda a b (+ a (if (has? b 2) 1 0))) 0)
         (array:reduce entries (lambda a b (+ a (if (has? b 3) 1 0))) 0)))))
 
-(let PARSED (parse INPUT))
-
-(let INPUT2 "abcde
-fghij
-klmno
-pqrst
-fguij
-axcye
-wvxyz")
-
 (let part2 (lambda input 
     (|> input
         (array:unique-pairs)
         (array:map pair:zip)
         (array:select (lambda pairs 
-            (= (array:count-of pairs (lambda [a b .] (<> a b))) 1)))
+            (= (array:count-of pairs (lambda [ a b . ] (<> a b))) 1)))
         (array:flat-one)
-        (array:exclude (lambda [a b .] (<> a b)))
+        (array:exclude (lambda [ a b . ] (<> a b)))
         (pair:unzip)
         (array:first))))
 
-(let PARSED2 (parse INPUT2))
+[
+(part1 
+(parse "abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab")) 
 
-[(part1 PARSED) (part2 PARSED2)]
+(part2 (parse "abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz"))
+]
