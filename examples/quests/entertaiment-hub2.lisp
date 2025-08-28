64f89673509beae5f15456634f0d7acccb6e00e5
Anthony <anthony@Anthonys-MacBook-Pro.local>
2025-08-28 14:20:13 +0300

    Opt example

--- examples/quests/entertaiment-hub2.lisp
+++ examples/quests/entertaiment-hub2.lisp
@@ -2,12 +2,12 @@
 
 (let fluffbolts [char:R char:G char:B])
 
-(let shoot (lambda i balloons (do 
+(let tail-call:shoot (lambda i balloons (do 
       (let fluff (array:get fluffbolts (mod i (array:length fluffbolts))))
       (let baloon (array:pop! balloons))
       (cond 
         (array:empty? balloons) (+ i 1) 
-        (= fluff baloon) (shoot i balloons)
-        (*) (shoot (+ i 1) balloons)))))
+        (= fluff baloon) (tail-call:shoot i balloons)
+        (*) (tail-call:shoot (+ i 1) balloons)))))
 
-[(shoot 0 (math:reverse balloons))]
+[(tail-call:shoot 0 (math:reverse balloons))]
