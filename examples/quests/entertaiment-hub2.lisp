4464032f65a50acd8f09f0449f70efdfe2d8fac2
Anthony <anthony@Anthonys-MacBook-Pro.local>
2025-08-28 14:13:47 +0300

    Update example

--- examples/quests/entertaiment-hub2.lisp
+++ examples/quests/entertaiment-hub2.lisp
@@ -3,7 +3,7 @@
 (let fluffbolts [char:R char:G char:B])
 
 (let shoot (lambda i balloons (do 
-      (let fluff (get fluffbolts (mod i (length fluffbolts))))
+      (let fluff (array:get fluffbolts (mod i (length fluffbolts))))
       (let baloon (array:pop! balloons))
       (cond 
         (array:empty? balloons) (+ i 1) 
